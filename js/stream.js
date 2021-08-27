86c73f7bb81324001440da13206e7601fde3b34d
rabans <bansal.rahul14@gmail.com>
2021-08-27 11:18:54 +0530

    added MetaEditor

--- js/stream.js
+++ js/stream.js
@@ -1,78 +1,70 @@
-const decoder = new EBML.Decoder();
-const reader = new EBML.Reader();
-reader.logging = true;
+class MetaEditor {
 
-let tasks = Promise.resolve(void 0);
-let segmentOffset = 0;
-let metadataElms = [];
-let metadataSize = 0;
-let webM = new Blob([], {type: "video/webm"});
-let last_duration = 0;
-const cue_points = [];
-
-
-// const stream = await navigator.mediaDevices.getUserMedia({video: true, audio: true});
-// const rec = new MediaRecorder(stream, { mimeType: 'video/webm; codecs="vp8, opus"'});
-
-reader.addListener("segment_offset", (offset)=>{
-  segmentOffset = offset;
-});
-
-reader.addListener("metadata", ({data, metadataSize: size})=>{
-    metadataElms = data;
-    metadataSize = size;
-  });
-
-reader.addListener("duration", ({timecodeScale, duration})=>{
-  last_duration = duration;
-});
-
-reader.addListener("cue_info", ({CueTrack, CueClusterPosition, CueTime})=>{
-  cue_points.push({CueTrack, CueClusterPosition, CueTime});
-})
-
-
-function readAsArrayBuffer(blob) {
-    return new Promise((resolve, reject)=>{
-      const reader = new FileReader();
-      reader.readAsArrayBuffer(blob);
-      reader.onloadend = ()=>{ resolve(reader.result); };
-      reader.onerror = (ev)=>{ reject(ev.error); };
-    });
-  }
-
-
-  const createMetaInfo = (chunk) => {
-    console.log('In the createMetaInfo for the chunk', chunk);
-    const task = async ()=>{
-        const buf = await readAsArrayBuffer(chunk);
-        const elms = decoder.decode(buf);
-        elms.forEach((elm)=>{ reader.read(elm); });
-        console.log('In the task method for the chunk', chunk);
-    };
-    tasks = tasks.then(()=> task() );
-    return tasks;
-}
-
-const TrimVideo = async (chunks) => {
-    for (let chunk in chunks){
-        await createMetaInfo(chunks[chunk]);
+    constructor(){
+        this.decoder = new EBML.Decoder();
+        this.readerEBML = new EBML.Reader();
+        this.readerEBML.logging = true;
+        this.tasks = Promise.resolve(void 0);
+        this.segmentOffset = 0;
+        this.metadataElms = [];
+        this.metadataSize = 0;
+        this.webM = new Blob([], {type: "video/webm"});
+        this.last_duration = 0;
+        this.cue_points = [];
     }
-    return new Promise((resolve, reject) => {
-      setTimeout(async () => {
-        const refinedMetadataBuf = EBML.tools.makeMetadataSeekable(reader.metadatas, reader.duration, reader.cues);
-        const zeroChunk = await modifyMetadata(chunks[0], reader.metadataSize, refinedMetadataBuf)
-        chunks[0] = zeroChunk;
-        // return chunks;
-        resolve(chunks);
-      }, 3000)
-    })
-}
 
-const modifyMetadata = async (chunk, metadataSize, refinedMetadataBuf) => {
-    const webMBuf = await readAsArrayBuffer(chunk);
-    // There is possibility of pre append the metadata instead of making a slice
-    const body = webMBuf.slice(metadataSize)
-    const refinedWebM = new Blob([refinedMetadataBuf, body], {type: webM.type});
-    return refinedWebM
+    readAsArrayBuffer = async(blob)  => {
+        return new Promise((resolve, reject)=>{
+        const reader = new FileReader();
+            reader.readAsArrayBuffer(blob);
+            reader.onloadend = ()=>{ resolve(reader.result); };
+            reader.onerror = (ev)=>{ reject(ev.error); };
+        });
+    }
+
+    modifyMetadata = async (chunk, metadataSize, refinedMetadataBuf) => {
+        const webMBuf = await this.readAsArrayBuffer(chunk);
+        // There is possibility of pre append the metadata instead of making a slice
+        const body = webMBuf.slice(metadataSize)
+        const refinedWebM = new Blob([refinedMetadataBuf, body], {type: webMBuf.type});
+        return refinedWebM
+    }
+
+    createMetaInfo = async (chunk) => {
+        console.log('In the createMetaInfo for the chunk', chunk);
+        const task = async ()=>{
+            const buf = await this.readAsArrayBuffer(chunk);
+            const elms = this.decoder.decode(buf);
+            elms.forEach((elm)=>{ this.readerEBML.read(elm); });
+            console.log('In the task method for the chunk', chunk);
+        };
+        this.tasks = this.tasks.then(()=> task() );
+        return this.tasks;
+    }
+
+    combineChunks = (chunks) => {
+        return new Blob(chunks, {
+            type: 'video/webm'
+        });
+    }
+
+    generateSeekableVideo = async (chunks) => {
+        for (let chunk of chunks){
+            await this.createMetaInfo(chunk);
+        }
+        this.readerEBML.stop();
+        const refinedMetadataBuf = EBML.tools.makeMetadataSeekable(this.readerEBML.metadatas, this.readerEBML.duration, this.readerEBML.cues);
+        const zeroChunk = await this.modifyMetadata(chunks[0], this.readerEBML.metadataSize, refinedMetadataBuf)
+        chunks[0] = zeroChunk;
+        return this.combineChunks(chunks)
+    }
+
+    printEBMLJson = async (webmBlob) => {
+        const webmBuff = await this.readAsArrayBuffer(webmBlob);
+        const ebmlToJson = new EbmlToJson(webmBuff);
+        const json = ebmlToJson.toString()
+        console.log('The json is', json);
+        return json;
+    }
+
 }
